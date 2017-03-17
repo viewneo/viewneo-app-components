8d00b11d128b8cd14d139cc0a822c57391821f71
chams <chamseddine.boussaid@adversign-media.de>
2017-03-17 16:52:46 +0100

    Updated index.js

--- index.js
+++ index.js
@@ -1,21 +1,35 @@
 'use strict';
 
-var _main = require('./build/static/js/main');
-//var css = require('./build/static/css/main.css');
+const {
+  Container,
+  Row,
+  FormGroup,
+  InputText,
+  Button,
+  Col,
+  Hint,
+  ColorInput,
+  CheckboxInput,
+  TextInput,
+  SubmitButton,
+  SelectInput,
+  TextArea,
+  DeleteButton
+} = require('./build/static/js/main').default;
 
 module.exports = {
-  Container: _main.default.Container,
-  Row: _main.default.Row,
-  FormGroup: _main.default.FormGroup,
-  InputText: _main.default.InputText,
-  Button: _main.default.Button,
-  Col: _main.default.Col,
-  Hint: _main.default.Hint,
-  ColorInput: _main.default.ColorInput,
-  CheckboxInput: _main.default.CheckboxInput,
-  TextInput: _main.default.TextInput,
-  SubmitButton: _main.default.SubmitButton,
-  SelectInput: _main.default.SelectInput,
-  TextArea: _main.default.TextArea,
-  DeleteButton: _main.default.DeleteButton
+  Container: Container,
+  Row: Row,
+  FormGroup: FormGroup,
+  InputText: InputText,
+  Button: Button,
+  Col: Col,
+  Hint: Hint,
+  ColorInput: ColorInput,
+  CheckboxInput: CheckboxInput,
+  TextInput: TextInput,
+  SubmitButton: SubmitButton,
+  SelectInput: SelectInput,
+  TextArea: TextArea,
+  DeleteButton: DeleteButton
 };
